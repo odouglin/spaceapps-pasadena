9697db32ce1b0a7b939510c0db7e439e2ff91418
Jacqueline Friedberg <77143564+JacquelineFriedberg@users.noreply.github.com>
2021-08-08 19:17:41 -0700

    added color changes, logo in nav, hero image

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Import all components
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCRipple } from '@material/ripple';
+import {MDCBanner} from '@material/banner';
+
 
 //
 // Instantiate all components
@@ -8,3 +10,4 @@ import { MDCRipple } from '@material/ripple';
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+const banner = new MDCBanner(document.querySelector('.mdc-banner'));
